d6ce25c4d288327aa06537a3cce5156435ef39a8
Ravi Singh <212677397@ge.com>
2018-09-04 20:08:04 +0530

    Updating app to include test case for running locally and assigning port

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
 import os
 
-port = int(os.getenv("PORT"))
+#port = int(os.getenv("PORT"))
+#checking if running locally or in cf. if locally assign port 8080 to the webapp
+if os.getenv("PORT") is None:
+  port = int(8080)
+else: 
+  port = int(os.getenv("PORT"))
 app = Flask(__name__)
 
 
